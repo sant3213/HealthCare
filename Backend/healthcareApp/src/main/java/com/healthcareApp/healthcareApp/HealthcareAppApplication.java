a4a59feb5fa5decbb06d0e959ccc4214363e160e
scardenas <scardenas@cidenet.com.co>
2020-06-26 20:35:22 -0500

    Feature: Project created

---
+++ Backend/healthcareApp/src/main/java/com/healthcareApp/healthcareApp/HealthcareAppApplication.java
@@ -0,0 +1,13 @@
+package com.healthcareApp.healthcareApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcareAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthcareAppApplication.class, args);
+	}
+
+}
